50fc3b372307dc6e0bca89af90a313aaf5a0e7ba
seeeedn <jonas.m1304@gmail.com>
2024-11-13 16:27:58 +0100

    fixed notations

--- src/data/pokemon/level_up_learnsets.h
+++ src/data/pokemon/level_up_learnsets.h
@@ -3763,8 +3763,8 @@ static const u16 sMightyenaLevelUpLearnset[] = {
 };
 
 static const u16 sZigzagoonLevelUpLearnset[] = {
-    LEVEL_UP_MOVE( 1, MOVE_SCRATCH),
-    //LEVEL_UP_MOVE( 1, MOVE_GROWL),
+    LEVEL_UP_MOVE( 1, MOVE_TACKLE),
+    LEVEL_UP_MOVE( 1, MOVE_GROWL),
     LEVEL_UP_MOVE( 5, MOVE_TAIL_WHIP),
     LEVEL_UP_MOVE( 9, MOVE_HEADBUTT),
     LEVEL_UP_MOVE(13, MOVE_SAND_ATTACK),
@@ -5618,7 +5618,7 @@ static const u16 sChimechoLevelUpLearnset[] = {
 
 static const u16 sZekromLevelUpLearnset[] = {
     LEVEL_UP_MOVE( 1, MOVE_THUNDER_PUNCH),
-    LEVEL_UP_MOVE( 1 ,MOVE_THUNDER_WAVE),
+    LEVEL_UP_MOVE( 1, MOVE_THUNDER_WAVE),
     LEVEL_UP_MOVE( 1, MOVE_THUNDER_SHOCK),
     LEVEL_UP_END
 };
